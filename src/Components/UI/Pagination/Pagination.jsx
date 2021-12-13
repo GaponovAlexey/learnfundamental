ff857d684aa12db1ebf81104f55c65835c149f82
kekelberi <alexei.gaponov@gmail.com>
2021-12-13 19:26:30 +0300

    save

--- src/Components/UI/Pagination/Pagination.jsx
+++ src/Components/UI/Pagination/Pagination.jsx
@@ -3,10 +3,15 @@ import { getPagesArray } from '../../../utils/page'
 
 export const Pagination = ({ totalPage, page, changePage }) => {
   let pageArray = getPagesArray(totalPage)
+
   return (
-    <div>
+    <div className='page__wraper'>
       {pageArray.map((p) => (
-        <span onClick={() => changePage(p)} key={p}>
+        <span
+          className={page === p ? 'page page__active' :'page'}
+          onClick={() => changePage(p)}
+          key={p}
+        >
           {p}
         </span>
       ))}
