73c566bdb621884061b7cce673de46f05dec088f
kekelberi <alexei.gaponov@gmail.com>
2021-12-17 17:16:51 +0300

    save

--- src/Components/UI/Navbar.jsx/Navbar.jsx
+++ src/Components/UI/Navbar.jsx/Navbar.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { MyContext } from '../../Context/Context'
 
 export const Navbar = () => {
+  const { setISAUTH, isAuth } = useContext(MyContext)
+  const relog = (e) => {
+    // e.preventDefault()
+    setISAUTH(!isAuth)
+    localStorage.removeItem('auth')
+  }
   return (
     <div>
       <div className='navbar'>
         <div className='navbar__links'>
-          <Link to='/about' >o сайте</Link>
-          <Link  to='/posts' >Посты</Link>
+          {isAuth ? (
+            <button onClick={() => relog()}>Relog</button>
+          ) : (
+            <button>login</button>
+          )}
+          <Link to='/about'>o сайте</Link>
+          <Link to='/posts'>Посты</Link>
         </div>
       </div>
     </div>
